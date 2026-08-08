44c663ee358fd24ec257713075e32b41c7046436
maksnento-dotcom <maksnento@gmail.com>
2026-08-08 17:52:08 +0300

    Update bot_bolshevik.py

--- bot_bolshevik.py
+++ bot_bolshevik.py
@@ -10,7 +10,7 @@ from aiogram.filters import Command
 from aiogram.types import BotCommand
 from aiogram.enums import ParseMode
 
-TOKEN = "8725576726:AAH1Ey6ZFK4DFB5pBYPfoYwRzV-vMScsjNI"
+TOKEN = "8725576726:AAG8qfH0hzkM_Z7EpVJKw8t-WZm0lJbmiGs"
 
 KILLCOOLDOWN = 3600 
 
